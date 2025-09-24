c58b9d563fd3fb8e6c2159a5ce9210d4122485d4
Alexandra Yaschuk <alexandrayaschuk1995@gmail.com>
2025-09-24 11:56:51 +0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { store } from "./app/store.ts";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <StrictMode>
-      <BrowserRouter basename="/youtube-spa/">
+      <HashRouter basename="/youtube-spa/">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>
   </Provider>
 );
